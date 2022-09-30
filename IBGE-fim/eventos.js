c267f5aace688fd026bbbf811bc84db9b6e8d379
AdriellySoares <113360657+AdriellySoares@users.noreply.github.com>
2022-09-30 12:59:53 -0300

    add ibge finalizado

---
+++ IBGE-fim/eventos.js
@@ -0,0 +1,40 @@
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
+    .then((resposta)=> resposta.json())
+    .then((regioes)=>{
+        regioes.map((cadaRegiao)=>{
+            document.getElementById('regiao').innerHTML +=`
+            <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>
+            `;
+        });
+    });
+    function mostrarEstado(){
+            document.getElementById('estado').innerHTML = `<option id="opcaoEstado" value="0">--Selecione--</option>`;
+            let regiaoSelecionada = regiao.value;
+            fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regiaoSelecionada}/estados`)
+            .then((resposta)=> resposta.json())
+            .then((estados)=>{
+                let estadoString = document.getElementById('estado').innerHTML;
+                estados.map((cadaEstado)=>{
+                    estadoString +=`
+                    <option value="${cadaEstado.id}">${cadaEstado.nome}</option>
+                `;
+            });
+            document.getElementById('estado').innerHTML = estadoString;
+        });
+    }
+    function mostrarCidade(){
+        document.getElementById('cidade').innerHTML = `<option id="opcaocidade" value="0">--Selecione--</option>`;
+            let estadoSelecionado = estado.value;
+            fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/municipios`)
+            .then( resposta => resposta.json())
+            .then((cidades)=>{
+                console.log(cidades);
+                let cidadeString = document.getElementById('cidade').innerHTML;
+                cidades.map((cadaCidade)=>{
+                    cidadeString +=`
+                    <option value="${cadaCidade.id}">${cadaCidade.nome}</option>
+                `;
+            });
+            document.getElementById('cidade').innerHTML = cidadeString;
+        });
+    }
